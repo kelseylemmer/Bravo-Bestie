b394696778957397c54c0bf79b207fc4bf4346d0
Kelsey Lemmer <kelseylemmer@gmail.com>
2023-09-18 21:58:54 -0400

    home page

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -4,8 +4,17 @@ import "./home.css";
 
 export const Home = () => {
   return (
-    <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', alignContent: 'center' }}>
-      <img src="https://i.imgur.com/cnD6p2ot.png" alt="Bravo Bestie" className="home-pic" />
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        alignContent: 'center',
+        height: '100vh',
+        width: '100%',
+      }}
+    >
+      <img src="https://i.imgur.com/aqAkDpE.png" alt="Bravo Bestie" className="home-pic" />
     </Box>
-  )
-}
+  );
+};
